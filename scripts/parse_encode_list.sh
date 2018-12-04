2eaf642cde6a5b8e66c04bf51f3f19c70cb1f2b9
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-12-04 09:27:15 +0100

    first commit

---
+++ scripts/parse_encode_list.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# preliminaries
+datadir="../data"
+datafile="$datadir/test-encode-list.txt"
+metadata="$datadir/metadata.txt"
+
+# first line is the metadata: download
+curl -o $metadata $(head -n1 $datafile)
+
+# parse the metadata file: extract only the lines that contain tsv files
+# that have a "gene quantifications" identifier
+to_download=$(grep "gene quantifications" $metadata | grep .tsv | awk '{ print $1 }')
+
+# the "to_download" list contains a series of files that correspond to URLs that
+# we can fetch then with cURL
+for id in $to_download; do
+  file=$(grep $id $datafile)
+  curl -L $file -o $datadir/$id.tsv
+done
