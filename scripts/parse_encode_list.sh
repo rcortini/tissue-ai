b01607a2e53b9f87049a8d4ed436e45b4363089f
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-12-04 10:18:39 +0100

    put log in parse_encode_list

--- scripts/parse_encode_list.sh
+++ scripts/parse_encode_list.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+function time_string {
+  echo "[`date +"%Y-%m-%d %H:%M:%S"`]"
+}
+
+function log_message {
+  echo "`time_string` `basename $0`: INFO: $1"
+}
+
 # preliminaries
 datadir="../data"
 datafile="$datadir/files.txt"
 metadata="$datadir/metadata.txt"
 
 # first line is the metadata: download
+log_message "Downloading metadata"
 curl -o $metadata $(head -n1 $datafile)
 
 # parse the metadata file: extract only the lines that contain tsv files
@@ -16,5 +25,8 @@ to_download=$(grep "gene quantifications" $metadata | grep .tsv | awk '{ print $
 # we can fetch then with cURL
 for id in $to_download; do
   file=$(grep $id $datafile)
+  log_message "EXPERIMENT ID: $id"
   curl -L $file -o $datadir/$id.tsv
 done
+
+log_message "Done"
