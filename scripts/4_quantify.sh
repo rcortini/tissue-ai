01f484749aa05f11290b7254db5afddc8ffe1819
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-12-18 11:12:11 +0100

    working 4_quantify.sh

--- scripts/4_quantify.sh
+++ scripts/4_quantify.sh
@@ -3,13 +3,14 @@
 source 0_general_tools.sh
 
 # parse command line
-if [ $# -lt 2 ]; then
-  echo "Usage: 4_quantify.sh <experiment_dir> <metadata>" 1>&2
+if [ $# -lt 3 ]; then
+  echo "Usage: 4_quantify.sh <experiment_dir> <metadata> <salmon_index>" 1>&2
   exit 1
 fi
 
 experiment_dir=$1
 metadata=$2
+salmon_index=$3
 
 # cd to directory
 root_dir=$(pwd)
@@ -25,9 +26,9 @@ while [ -e $fname ]; do
   # parse the replicate file
   run_type=$(head -n 1 $fname)
   if [ "$run_type" = "paired-ended" ]; then
-    runs=$(tail -n 2 $fname | awk '{ print $1 }')
+    runs=($(tail -n 2 $fname | awk '{ print $1 }'))
   elif [ "$run_type" = "single-ended" ]; then
-    runs=$(tail -n 1 $fname | awk '{ print $1 }')
+    runs=($(tail -n 1 $fname | awk '{ print $1 }'))
   fi
   
   # before anything, check that all the downloaded files
@@ -35,14 +36,28 @@ while [ -e $fname ]; do
   md5sum_fname="replicate-$count.md5sum"
   md5sum_log="replicate-$count.md5sum.log"
   rm -f $md5sum_fname
-  for run in $runs; do
+  for run in ${runs[@]}; do
     sum=$(get_md5sum "$root_dir/$metadata" $run)
     echo "$sum $run.fastq.gz" >> $md5sum_fname
   done
   md5sum -c $md5sum_fname > $md5sum_log
 
-  # if failed, exit
+  # proceed to quant step only if everything is okay
   if [ $? -eq 0 ]; then
+
+    # this step is the crucial one: notice that the LIB_TYPE argument (-l) given
+    # to salmon has been chosen to A, so that salmon will try to automatically
+    # figure out what type of library it is
+    if [ "$run_type" = "paired-ended" ]; then
+      salmon quant -i $salmon_index -l A \
+	-1 ${runs[0]}.fastq.gz -2 ${runs[1]}.fastq.gz \
+	-o replicate-$count-quant --validateMappings
+    elif [ "$run_type" = "single-ended" ]; then
+      salmon quant -i $salmon_index -l A \
+	-1 ${runs[0]}.fastq.gz -2 ${runs[1]}.fastq.gz \
+	-o replicate-$count-quant --validateMappings
+    fi
+   
   else
     echo "md5sum check failed!"
     cat $md5sum_log
