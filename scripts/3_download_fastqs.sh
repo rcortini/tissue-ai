c358b9572537c5429b01d7cecfa9baa7d2f31788
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-12-19 10:37:56 +0100

    moved md5sum check to 3_download_fastqs

--- scripts/3_download_fastqs.sh
+++ scripts/3_download_fastqs.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+function do_md5sum_test {
+  # get arguments
+  md=$1
+  my_run=$2
+
+  # prepare the md5sum test
+  md5sum_fname=$(mktemp)
+  sum=$(get_md5sum $md $my_run)
+  echo "$sum $my_run.fastq.gz" > $md5sum_fname
+  md5sum -c $md5sum_fname
+}
+
 # parse command line
-if [ $# -lt 1 ]; then
-  echo "Usage: download_and_quantify.sh <experiment_dir>" 1>&2
+if [ $# -lt 3 ]; then
+  echo "Usage: download_and_quantify.sh <tissue_ai_home> <experiment_dir> <metadata>" 1>&2
   exit 1
 fi
 
-experiment_dir=$1
+tissue_ai_home=$1
+experiment_dir=$2
+metadata=$3
+
+source $tissue_ai_home/scripts/0_general_tools.sh
 
 # cd to directory
 root_dir=$(pwd)
@@ -22,19 +38,52 @@ while [ -e $fname ]; do
   # parse the replicate file
   run_type=$(head -n 1 $fname)
   if [ "$run_type" = "paired-ended" ]; then
-    runs=$(tail -n 2 $fname | awk '{ print $2 }')
+    runs=($(tail -n 2 $fname | awk '{ print $1 }'))
+    run_urls=($(tail -n 2 $fname | awk '{ print $2 }'))
   elif [ "$run_type" = "single-ended" ]; then
-    runs=$(tail -n 1 $fname | awk '{ print $2 }')
+    runs=$(tail -n 1 $fname | awk '{ print $1 }')
+    run_urls=$(tail -n 1 $fname | awk '{ print $2 }')
   fi
-  
-  # download
-  for run in $runs; do
 
-    # no-verbose option wget
-    wget -nv $run
+  # iterate over all runs of the replicate
+  for run in ${runs[@]}; do
 
+    echo $run
+
+    # check if files exist already
+    run_fname="$run.fastq.gz"
+    if test -e $run_fname; then
+
+      # if the file exists already, then we do the md5sum test
+      do_md5sum_test $metadata $run
+
+      # if test succeeded, it means that the file is already downloaded and we
+      # can exit
+      if [ $? -eq 0 ]; then
+	exit 0
+      else
+	# if md5sum check failed on the file that already exists, then the most
+	# likely explanation is that it was an incomplete download. In this case
+	# we download it all over
+	wget -nv $run
+      fi
+    else
+
+      # if it does not exist, we download it
+      wget -nv $run
+
+      # after complete, we check for integrity
+      do_md5sum_test $metadata $run
+
+      # check that the test succeeded
+      if [ $? -ne 0 ]; then
+	# in this case something went seriously wrong, and we quit
+	echo "Error: Corrupt FASTQ file for run $run" 1>&2
+	exit 1
+      fi
+    fi
   done
-
+  
   # increment counter and file name
   let count=count+1
   fname="replicate-$count.info"
--- scripts/4_quantify.sh
+++ scripts/4_quantify.sh
@@ -33,44 +33,24 @@ while [ -e $fname ]; do
     runs=($(tail -n 1 $fname | awk '{ print $1 }'))
   fi
   
-  # before anything, check that all the downloaded files
-  # have a correct md5sum
-  md5sum_fname="replicate-$count.md5sum"
-  md5sum_log="replicate-$count.md5sum.log"
-  rm -f $md5sum_fname
-  for run in ${runs[@]}; do
-    sum=$(get_md5sum "$root_dir/$metadata" $run)
-    echo "$sum $run.fastq.gz" >> $md5sum_fname
-  done
-  md5sum -c $md5sum_fname > $md5sum_log
+  # log file
+  salmon_log="replicate-$count-salmon.log"
 
-  # proceed to quant step only if everything is okay
-  if [ $? -eq 0 ]; then
-
-    # log file
-    salmon_log="replicate-$count-salmon.log"
-
-    # this step is the crucial one: notice that the LIB_TYPE argument (-l) given
-    # to salmon has been chosen to A, so that salmon will try to automatically
-    # figure out what type of library it is
-    if [ "$run_type" = "paired-ended" ]; then
-      salmon quant -i $salmon_index -l A \
-	-1 ${runs[0]}.fastq.gz -2 ${runs[1]}.fastq.gz \
-	--threads $nthreads \
-	-o replicate-$count-quant --validateMappings > $salmon_log
-    elif [ "$run_type" = "single-ended" ]; then
-      salmon quant -i $salmon_index -l A \
-	--threads $nthreads \
-	-r ${runs[0]}.fastq.gz  \
-	-o replicate-$count-quant --validateMappings > $salmon_log
-    fi
-   
-  else
-    echo "md5sum check failed!"
-    cat $md5sum_log
-    exit 1
+  # this step is the crucial one: notice that the LIB_TYPE argument (-l) given
+  # to salmon has been chosen to A, so that salmon will try to automatically
+  # figure out what type of library it is
+  if [ "$run_type" = "paired-ended" ]; then
+    salmon quant -i $salmon_index -l A \
+      -1 ${runs[0]}.fastq.gz -2 ${runs[1]}.fastq.gz \
+      --threads $nthreads \
+      -o replicate-$count-quant --validateMappings > $salmon_log
+  elif [ "$run_type" = "single-ended" ]; then
+    salmon quant -i $salmon_index -l A \
+      --threads $nthreads \
+      -r ${runs[0]}.fastq.gz  \
+      -o replicate-$count-quant --validateMappings > $salmon_log
   fi
-
+ 
   # increment counter and file name
   let count=count+1
   fname="replicate-$count.info"
